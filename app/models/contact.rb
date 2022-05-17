3d198da7dd65cbbbcd5e35db7b7105ad68a803af
Nilton <ni.flavio@hotmail.com>
2022-05-17 15:09:57 -0300

    Customizing my JSON render

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+    def author
+        "Nilton Lima"
+    end
+
+    def as_json(options={})
+        super(methods: :author, root: true, only: [:name, :email])
+    end
 end
